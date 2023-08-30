89b06f596b26fa197e5fcb568e98b66c644f428e
Jarek Jurasz <jjurasz@moba.de>
2023-08-30 13:48:45 +0200

    Fixed parse error (see also https://github.com/justengel/isfreader/pull/1)

--- tests/test_isfreader.py
+++ tests/test_isfreader.py
@@ -1,16 +1,25 @@
+import sys, os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, '..')
+
+import isfreader
+import numpy as np
 
 
 def test_isfreader():
-    import isfreader
-    import numpy as np
-
     data = isfreader.read_file('./T0000CH1.ISF')
     np_data = np.load('./T0000CH1.npy')
     assert data.shape == np_data.shape
     assert data.dtype == np_data.dtype
     assert np.all(data == np_data)
 
+def test_data1():
+    data = isfreader.read_file('./data1.ISF')
+    assert (10000, 2) == data.shape
+    assert np.float == data.dtype
+
 
 if __name__ == '__main__':
     test_isfreader()
+    test_data1()
     print('All tests finished successfully!')
